aca12af50894c90819e0cae7689dea93625f127b
ShivasaiPothuganti <shivasai.1905l5@gmail.com>
2022-02-16 18:06:54 +0000

    iblocks

---
+++ iblocks-frontend/src/components/Devteam.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Devteam.css"
+function Devteam() {
+    return (
+        <div className="devteam">
+           <h1>Made by <span>Shivasai</span></h1> 
+        </div>
+    )
+}
+
+export default Devteam;
